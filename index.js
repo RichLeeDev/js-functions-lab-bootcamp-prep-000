291d7f76544cdd58f4583c400a3ca722640a8554
Richard Lee <richlee81192@gmail.com>
2019-05-25 20:34:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-happyHolidays(you)
+
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}"
 }
+
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!"
 }
+
 function holidayCountdown(holiday, days) {
   return "It\'s ${days} days until ${holiday}!"
 }
+
